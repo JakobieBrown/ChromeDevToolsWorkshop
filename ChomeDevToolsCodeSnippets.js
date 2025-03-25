ee5fd172fced2806bed4f3d7324eb63124f26018
Jakobie Brown <sjbrown1@eagles.usi.edu>
2025-03-25 08:12:37 -0500

    Add files via upload

---
+++ ChomeDevToolsCodeSnippets.js
@@ -0,0 +1,15 @@
+const p = document.createElement('p');
+
+p.textContent = 'Hello World';
+
+document.body.appendChild(p);
+
+const title = document.getElement
+
+const headers = document.querySelectorAll('h1');
+
+headers[0].style.color = "green"
+
+const button = document.getElementById("loadData")
+
+button.style.display = "block"
